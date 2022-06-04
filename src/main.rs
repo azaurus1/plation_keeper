a6aa3188f35f8b48b9c728843f8225cc63475ce2
Liam Aikin <liamaikin@gmail.com>
2022-06-04 14:13:46 +1000

    Getting Contract handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+extern crate tokio;
+
+use std::str::FromStr;
+use hex_literal::hex;
+use dotenv::dotenv;
+use web3::{Web3, futures::TryFutureExt};
+
+#[tokio::main]
+async fn main() -> web3::Result {
+    dotenv().ok();
+
+    //Env variables
+    let private_key = "PRIV_KEY";
+    let private_key = dotenv::var(private_key).unwrap();
+
+    let rpc_key = "RPC";
+    let rpc = dotenv::var(rpc_key).unwrap();
+
+    let prediction_handler_address = web3::types::Address::from_str("0x122dF77D0069667EFA788aD78650630cb7e8170d").unwrap();
+
+    let inflation_oracle = "INFLATION_ORACLE_ADDR";
+    let inflation_oracle_address = dotenv::var(inflation_oracle).unwrap();
+
+    //Web3 inits
+    let transport = web3::transports::Http::new(&rpc)?;
+    let web3 = web3::Web3::new(transport);
+    let address = web3::types::Address::from_str("0x0a29bd68d085CcFa30bDFf2c2ca849D5976a4C9D").unwrap();
+    let latest = Some(web3::types::BlockNumber::Latest);
+    let nonce = web3.eth().transaction_count(address, latest).await?;
+
+    let prediction_handler_bytecode = include_str!("./contracts/PredictionHandler.bin");
+    let prediction_handler_contract = web3::contract::Contract::from_json(web3.eth(),prediction_handler_address,include_bytes!("./contracts/PredictionHandler.abi"));
+
+    println!("{:?}",prediction_handler_address);
+    println!("{:?}",prediction_handler_contract);
+
+
+
+
+
+    Ok(())
+
 }
